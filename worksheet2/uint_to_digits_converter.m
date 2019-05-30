983a052dcd50ccaccc498ea1afee6e1b158f3cd9
mlesigues <marielle_lesigues029@yahoo.com>
2019-05-30 12:56:43 -0700

    Add files via upload

--- worksheet2/uint_to_digits_converter.m
+++ worksheet2/uint_to_digits_converter.m
@@ -1,1 +1,14 @@
-
+function X = uint_to_digits_converter(x)
+% a function that converts a natural number (nonnegative integer) 
+% into an array of its decimal digit
+% x = natural number
+% X = array of natural numbers, between 0 - 9
+    X = []; 
+    for g = ceil(log10(x)): -1:1
+	initialNum = floor(mod(x/(10^(g-1)),10));
+	X(end + 1, :) = (initialNum);
+    end
+    
+    %convert vertical array to horizontal using transpose
+        X = X';
+end

61a055bbfb895aad4bdd613ee84142e41715edd3
mlesigues <marielle_lesigues029@yahoo.com>
2019-05-30 13:16:57 -0700

    Add files via upload

--- worksheet4/fixed_point_itr.m
+++ worksheet4/fixed_point_itr.m
@@ -1,1 +1,16 @@
-
+function [p, psVec]  = fixed_point_itr(g, p0, maxits) %prof did this in class
+%g = function handle
+%p0 = initial iteration
+%maxits = # of iterations to perform
+%p = results of maxits applications of g
+    p = p0;
+    psVec = []; %to store p values, so we can use it to graph errors 
+    for i = 1:maxits
+        p = g(p); %this is on the worksheet. p = g(g(···g(p0)···)).
+       % psVec(i+1) = p;
+        psVec(i) = p;
+        fprintf('After %d iterations: %g\n',i,p)
+        %disp(p)
+        
+    end
+end
